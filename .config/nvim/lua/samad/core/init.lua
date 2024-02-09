c8684ec0f12e95706a21943254d525a677282c34
Samad Khafi <skhafi7@gmail.com>
2024-02-09 22:51:19 +0530

    feat: add pcss file extension as postcss filetype

--- .config/nvim/lua/samad/core/init.lua
+++ .config/nvim/lua/samad/core/init.lua
@@ -1,5 +1,8 @@
 require 'samad.core.keymaps'
 require 'samad.core.options'
 
+-- add postcss filetype
+vim.filetype.add { extension = { pcss = 'postcss' } }
+
 -- add templ filetype
 vim.filetype.add { extension = { templ = 'templ' } }
