72e31611d9adaa1bc75f197ec10eec732724737e
Samad Khafi <skhafi7@gmail.com>
2024-03-20 23:56:54 +0530

    fix(nvim): nvim-dap missing dependency & plugin updates

--- .config/nvim/lua/samad/plugins/dap/nvim-dap.lua
+++ .config/nvim/lua/samad/plugins/dap/nvim-dap.lua
@@ -2,6 +2,7 @@ return {
     'rcarriga/nvim-dap-ui',
     dependencies = {
         'mfussenegger/nvim-dap',
+        'nvim-neotest/nvim-nio',
         'theHamsta/nvim-dap-virtual-text',
     },
     config = function()
