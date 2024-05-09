0e6435aad23263bb7a88f07f0ae509e14cbd73e1
Samad Khafi <skhafi7@gmail.com>
2024-05-09 11:11:45 +0530

    fix(nvim): never show .git directory in neo-tree

--- .config/nvim/lua/samad/plugins/neo-tree.lua
+++ .config/nvim/lua/samad/plugins/neo-tree.lua
@@ -104,6 +104,7 @@ return {
                         'node_modules',
                     },
                     never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+                        '.git',
                         '.DS_Store',
                         'thumbs.db',
                     },
