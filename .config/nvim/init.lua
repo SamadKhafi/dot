073cd29b8dd3d53084e4cce8ba417ff998b922ab
Samad Khafi <skhafi7@gmail.com>
2024-05-25 21:10:58 +0530

    fix(nvim): tbl_add_reverse_lookup deprecation warning

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,10 @@
+-- fix deprecation warning in typescript-tools
+-- TODO: remove when typescript-tools gets update
+vim.tbl_add_reverse_lookup = function(tbl)
+    for k, v in pairs(tbl) do
+        tbl[v] = k
+    end
+end
+
 require 'samad.core'
 require 'samad.lazy'
