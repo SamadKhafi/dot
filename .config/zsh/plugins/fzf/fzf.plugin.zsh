bd758eec744e127ffbe04d4380cacb1f382803e5
Samad Khafi <skhafi7@gmail.com>
2024-08-05 12:35:50 +0530

    fix(zsh): source correct fzf files

--- .config/zsh/plugins/fzf/fzf.plugin.zsh
+++ .config/zsh/plugins/fzf/fzf.plugin.zsh
@@ -1,9 +1,3 @@
-# catppuccin_mocha colorscheme
-# export FZF_DEFAULT_OPTS=" \
-    # --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-    # --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-    # --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-
 # tokyonight_night colorscheme
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
 --color=fg:#c0caf5,bg:#1a1b26,hl:#ff9e64 \
@@ -11,6 +5,13 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
 --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
 --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a"
 
+# source file if it exists
+include() {
+    [[ -f "$1" ]] && source "$1"
+}
+
 # fzf completion & keybindings
-source /usr/share/fzf/completion.zsh
-source /usr/share/fzf/key-bindings.zsh
+include /usr/share/fzf/completion.zsh
+include /usr/share/fzf/key-bindings.zsh
+include /usr/share/doc/fzf/examples/completion.zsh
+include /usr/share/doc/fzf/examples/key-bindings.zsh
