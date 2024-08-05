d670b6a517c84729f462dfa30a55ca4573a31058
Samad Khafi <skhafi7@gmail.com>
2024-08-05 12:25:53 +0530

    fix(nvim): suppress unwanted fidget messages

--- .config/nvim/lua/samad/plugins/lsp/fidget.lua
+++ .config/nvim/lua/samad/plugins/lsp/fidget.lua
@@ -1,8 +1,10 @@
 return {
     'j-hui/fidget.nvim',
     opts = {
-        integration = {
-            ['nvim-tree'] = { enable = true },
+        progress = {
+            suppress_on_insert = true,
+            ignore_done_already = true,
+            ignore_empty_message = true,
         },
     },
 }
