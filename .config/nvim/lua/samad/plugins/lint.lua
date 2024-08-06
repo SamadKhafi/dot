c1a7af5e5656fd32f9c2367cb7e6fcc4e81079b7
Samad Khafi <skhafi7@gmail.com>
2024-08-06 11:27:24 +0530

    fix(nvim): temp fix eslint_d not finding flat config

--- .config/nvim/lua/samad/plugins/lint.lua
+++ .config/nvim/lua/samad/plugins/lint.lua
@@ -1,5 +1,9 @@
 return {
     'mfussenegger/nvim-lint',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'neovim/nvim-lspconfig',
+    },
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
         local lint = require 'lint'
@@ -11,15 +15,38 @@ return {
             go = { 'golangcilint' },
             gohtml = { 'djlint' },
             javascript = { 'eslint_d' },
-            javascriptreact = { 'eslint_d' },
             less = { 'stylelint' },
             php = { 'tlint' },
             python = { 'pylint' },
             sass = { 'stylelint' },
             scss = { 'stylelint' },
+            svelte = { 'eslint_d' },
             templ = { 'golangcilint' },
+            typescript = { 'eslint_d' },
             yaml = { 'yamllint' },
         }
+
+        -- begin: temporary eslint_d flat config fix --
+        local util = require 'lspconfig/util'
+        local path = require 'plenary.path'
+
+        local eslint = lint.linters.eslint_d
+        eslint.args = {
+            '--no-warn-ignored',
+            '--format',
+            'json',
+            '--config',
+            function()
+                local config_root = util.root_pattern 'eslint.config.js'(vim.api.nvim_buf_get_name(0))
+                return util.path.join(path:new(config_root):make_relative(vim.uv.cwd()), 'eslint.config.js')
+            end,
+            '--stdin',
+            '--stdin-filename',
+            function()
+                return vim.api.nvim_buf_get_name(0)
+            end,
+        }
+        -- end: temporary eslint_d flat config fix --
 
         local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
