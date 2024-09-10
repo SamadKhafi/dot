ec06485b66b6bc27b3de380a6ec9f8ef6a4dd569
Samad Khafi <skhafi7@gmail.com>
2024-09-10 12:27:32 +0530

    feat(nvim): use vscode icons for clangd ast

--- .config/nvim/lua/samad/plugins/lsp/clangd_extensions.lua
+++ .config/nvim/lua/samad/plugins/lsp/clangd_extensions.lua
@@ -1,7 +1,27 @@
 return {
     'https://git.sr.ht/~p00f/clangd_extensions.nvim',
     config = function()
-        require('clangd_extensions').setup {}
+        require('clangd_extensions').setup {
+            ast = {
+                role_icons = {
+                    type = '',
+                    declaration = '',
+                    expression = '',
+                    specifier = '',
+                    statement = '',
+                    ['template argument'] = '',
+                },
+                kind_icons = {
+                    Compound = '',
+                    Recovery = '',
+                    TranslationUnit = '',
+                    PackExpansion = '',
+                    TemplateTypeParm = '',
+                    TemplateTemplateParm = '',
+                    TemplateParamObject = '',
+                },
+            },
+        }
 
         -- C/C++ switch between header and source files
         vim.keymap.set('n', 'gh', '<cmd>ClangdSwitchSourceHeader<cr>', { desc = 'Switch to header or source' })
