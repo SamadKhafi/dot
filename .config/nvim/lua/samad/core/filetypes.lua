bd7fe01f194bad91dce20f0248aa9b88ee194b2f
Samad Khafi <skhafi7@gmail.com>
2024-11-13 09:32:08 +0530

    refactor(nvim): custom file types in separate file

---
+++ .config/nvim/lua/samad/core/filetypes.lua
@@ -0,0 +1,5 @@
+-- add postcss filetype
+vim.filetype.add { extension = { pcss = 'postcss' } }
+
+-- add markdown extended filetype
+vim.filetype.add { extension = { mdx = 'markdown.mdx' } }
--- .config/nvim/lua/samad/core/init.lua
+++ .config/nvim/lua/samad/core/init.lua
@@ -1,8 +1,3 @@
 require 'samad.core.keymaps'
 require 'samad.core.options'
-
--- add postcss filetype
-vim.filetype.add { extension = { pcss = 'postcss' } }
-
--- add markdown extended filetype
-vim.filetype.add { extension = { mdx = 'markdown.mdx' } }
+require 'samad.core.filetypes'
