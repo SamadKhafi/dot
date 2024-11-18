93b52c76ba327c2d8a7487399f37757b5dab7934
Samad Khafi <skhafi7@gmail.com>
2024-11-18 15:59:34 +0530

    fix(nvim): tailwindcss keymaps

--- .config/nvim/lua/samad/plugins/lsp/tailwind-tools.lua
+++ .config/nvim/lua/samad/plugins/lsp/tailwind-tools.lua
@@ -16,8 +16,23 @@ return {
 
         tailwind.setup(opts)
 
-        vim.keymap.set('n', '<leader>ut', '<cmd>TailwindConcealToggle<CR>', {
+        -- toggle tailwind conceal
+        vim.keymap.set('n', '<leader>uw', '<cmd>TailwindConcealToggle<CR>', {
             desc = '[UI] Toggle Tailwind Fold',
+            noremap = true,
+            silent = true,
+        })
+
+        -- goto previous tailwind classes
+        vim.keymap.set('n', '[w', '<cmd>TailwindPrevClass<CR>', {
+            desc = '[Tailwind] Previous Class',
+            noremap = true,
+            silent = true,
+        })
+
+        -- goto next tailwind classes
+        vim.keymap.set('n', ']w', '<cmd>TailwindNextClass<CR>', {
+            desc = '[Tailwind] Next Class',
             noremap = true,
             silent = true,
         })
