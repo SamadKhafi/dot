11f62bc07a157a3eb00d9cfcc79b4b0afe84fb61
Samad Khafi <skhafi7@gmail.com>
2024-12-30 14:09:59 +0530

    fix(nvim): set tokyonight colorscheme style

--- .config/nvim/lua/samad/plugins/tokyonight.lua
+++ .config/nvim/lua/samad/plugins/tokyonight.lua
@@ -4,7 +4,10 @@ return {
     priority = 1000,
     lazy = false,
     config = function()
-        require('tokyonight').setup {}
+        require('tokyonight').setup {
+            style = 'night',
+            transparent = false,
+        }
 
         vim.cmd.colorscheme 'tokyonight-night'
     end,
