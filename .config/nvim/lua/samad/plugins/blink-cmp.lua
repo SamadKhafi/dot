4e16db7f9eec8cd5816e5dd8d8611b94e233ed3d
Samad Khafi <skhafi7@gmail.com>
2025-07-14 16:07:27 +0530

    fix(nvim): use only buffer completion for comments

--- .config/nvim/lua/samad/plugins/blink-cmp.lua
+++ .config/nvim/lua/samad/plugins/blink-cmp.lua
@@ -79,7 +79,7 @@ return {
         sources = {
             default = function()
                 local success, node = pcall(vim.treesitter.get_node)
-                if success and node and vim.tbl_contains({}, node:type()) then
+                if success and node and vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }, node:type()) then
                     return { 'buffer' }
                 else
                     return { 'lsp', 'path', 'snippets', 'buffer' }
