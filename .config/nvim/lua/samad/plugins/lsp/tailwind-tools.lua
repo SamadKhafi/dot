05109a774ca1124326df3634608a410e552a1941
Samad Khafi <skhafi7@gmail.com>
2025-10-02 11:06:00 +0530

    fix(nvim): tailwind-tools lspconfig deprecated warning

--- .config/nvim/lua/samad/plugins/lsp/tailwind-tools.lua
+++ .config/nvim/lua/samad/plugins/lsp/tailwind-tools.lua
@@ -1,5 +1,5 @@
 return {
-    'luckasRanarison/tailwind-tools.nvim',
+    'richardhapb/tailwind-tools.nvim',
     dependencies = {
         'nvim-treesitter/nvim-treesitter',
         'neovim/nvim-lspconfig',
