93b3e0e40ba235f6f4bc403ef1f1e1e689879e83
Samad Khafi <skhafi7@gmail.com>
2026-03-01 10:34:53 +0530

    fix(nvim): migrate nvim-colorizer to new config

--- .config/nvim/lua/samad/plugins/colorizer.lua
+++ .config/nvim/lua/samad/plugins/colorizer.lua
@@ -1,5 +1,24 @@
 return {
-    'NvChad/nvim-colorizer.lua',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = true,
+    'catgoose/nvim-colorizer.lua',
+    event = { 'BufReadPre' },
+    opts = {
+        lazy_load = true,
+        options = {
+            parsers = {
+                css = true, -- preset: enables names, hex, rgb, hsl, oklch.
+                css_fn = true, -- preset: enables rgb, hsl, oklch
+                tailwind = {
+                    enable = true,
+                    lsp = true,
+                },
+                sass = { enable = true },
+            },
+            display = {
+                mode = 'background', -- 'background' | 'foreground' | 'virtualtext'
+                virtualtext = {
+                    position = 'before',
+                },
+            },
+        },
+    },
 }
