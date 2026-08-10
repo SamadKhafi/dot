f98cf008ee782e0394fd56765266df9f8fa57148
Samad Khafi <skhafi7@gmail.com>
2026-08-10 10:14:11 +0530

    fix(hypr): more env variables

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -29,9 +29,17 @@ end)
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
 hl.env('GTK_USE_PORTAL', '1')
-hl.env('QT_QPA_PLATFORM', 'wayland')
-hl.env('QT_QPA_PLATFORMTHEME', 'gtk3')
-hl.env('QT_QPA_PLATFORMTHEME_QT6', 'gtk3')
+hl.env('GDK_BACKEND', 'wayland,x11,*')
+hl.env('QT_QPA_PLATFORM', 'wayland;xcb')
+hl.env('QT_QPA_PLATFORMTHEME', 'qt6ct')
+hl.env('QT_QPA_PLATFORMTHEME_QT6', 'qt6ct')
+hl.env('QT_AUTO_SCREEN_SCALE_FACTOR', '1')
+hl.env('QT_WAYLAND_DISABLE_WINDOWDECORATION', '1')
+hl.env('XDG_CURRENT_DESKTOP', 'Hyprland')
+hl.env('XDG_SESSION_TYPE', 'wayland')
+hl.env('XDG_SESSION_DESKTOP', 'Hyprland')
+hl.env('SDL_VIDEODRIVER', 'wayland')
+hl.env('CLUTTER_BACKEND', 'wayland')
 hl.env('ELECTRON_OZONE_PLATFORM_HINT', 'auto')
 
 -----------------------
